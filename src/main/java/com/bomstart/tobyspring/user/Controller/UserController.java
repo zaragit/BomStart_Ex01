c1e7df49ed361428384f6060f395c7f5acaab2a0
Youngjin Mo <dev.youngjinmo@gmail.com>
2021-03-23 17:58:24 +0900

    refactor. 서비스 로직 리팩토링

--- src/main/java/com/bomstart/tobyspring/user/Controller/UserController.java
+++ src/main/java/com/bomstart/tobyspring/user/Controller/UserController.java
@@ -27,14 +27,7 @@ public class UserController {
 
     @PostMapping("/create")
     public @ResponseBody void createUser(User user) {
-        if(userService.getUser(user.getId())==null){
-            userService.createUser(user);
-            System.out.println("회원을 추가하였습니다.");
-        } else {
-            System.out.println("해당 id를 가진 회원이 있어서 회원정보를 수정합니다.");
-            userService.updateUser(user);
-            System.out.println("회원을 추가하였습니다.");
-        }
+        userService.createUser(user);
     }
 
     @PutMapping("/update")
@@ -44,12 +37,7 @@ public class UserController {
 
     @DeleteMapping("/delete/{id}")
     public @ResponseBody void deleteUser(@PathVariable String id){
-        if(userService.getUser(id)!=null){
-            userService.deleteUser(id);
-            System.out.println("회원 정보를 삭제했습니다.");
-        } else {
-            System.out.println("해당 id를 가진 회원이 존재하지 않습니다.");
-        }
+        userService.deleteUser(id);
     }
 
 }
--- src/main/java/com/bomstart/tobyspring/user/service/UserServiceImpl.java
+++ src/main/java/com/bomstart/tobyspring/user/service/UserServiceImpl.java
@@ -27,9 +27,11 @@ public class UserServiceImpl implements UserService{
     @Override
     public void createUser(User user) {
         if(userDao.selectUser(user.getId())!=null){
-            System.out.println("이미 존재하는 id 입니다.");
+            System.out.println("이미 등록된 회원입니다.");
+        } else {
+            System.out.println("회원 등록을 완료하였습니다.");
+            this.userDao.createUser(user);
         }
-        this.userDao.createUser(user);
     }
 
     @Override
@@ -40,6 +42,11 @@ public class UserServiceImpl implements UserService{
 
     @Override
     public void deleteUser(String id) {
-        this.userDao.deleteUser(id);
+        if(userDao.selectUser(id)==null){
+            System.out.println("등록되지 않은 회원입니다.");
+        } else {
+            System.out.println("회원을 삭제했습니다.");
+            this.userDao.deleteUser(id);
+        }
     }
 }
